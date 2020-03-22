12ae800f7200c092258bbef91ecd0eedbcddb089
Kevin <kevinkriegel07@gmail.com>
2020-03-22 22:14:21 +0100

    Post it maj

--- js/CPostit.js
+++ js/CPostit.js
@@ -1,13 +1,14 @@
 class Postit {
-    backgroundColor;
-    heigth;
-    width;
-    textColor;
-    textContent;
-    positionX;
-    positionY;
-    changeColor(colo) {
-        this.color = colo;
+    x;
+    y;
+    couleur;
+    text;
+
+    constructor(x, y, couleur, text) {
+        this.x = x;
+        this.y = y;
+        this.couleur = couleur;
+        this.text = text;
     }
 
     changePlace(x, y) {
@@ -15,25 +16,30 @@ class Postit {
         this.y = y;
     }
 
-    changeTextColor(){
-
+    changeColor(coul) {
+        this.couleur = coul;
     }
+
     afficheTest() {
-        let monElem = document.createElement('div')
+        let monElem = document.createElement("div");
+        monElem.className = "notes";
         monElem.style.position = "fixed";
         monElem.style.top = this.y + "px";
         monElem.style.left = this.x + "px";
-        monElem.style.width = "150px";
+        monElem.style.width = "250px";
         monElem.style.height = "150px";
         monElem.style.backgroundColor = this.couleur;
         monElem.style.padding = "5px";
         monElem.style.color = "black";
-        monElem.innerHTML = "Je suis un postit" ;
+        monElem.innerHTML = ` <h3> ${this.text} </h3><p>test postit</p>`;
         document.body.appendChild(monElem);
     }
 }
-monTest = new Postit(100, 200, 30, 'red');
-monTest.afficheTest();
+
+monTest1 = new Postit(435, 100, "blue", "Post ");
+monTest2 = new Postit(435, 300, "red", "Post ");
+monTest3 = new Postit(435, 500, "yellow", "Post");
+
 
 
 
