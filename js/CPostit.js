b6c7a4cb2827a416431b0f83f78cb40f26e4e6d6
Kevin <kevinkriegel07@gmail.com>
2020-03-24 23:23:09 +0100

    maj

--- js/CPostit.js
+++ js/CPostit.js
@@ -1,49 +1,142 @@
 class Postit {
-    x;
-    y;
+    posX;
+    posY;
     couleur;
     text;
 
-    constructor(x, y, couleur, text) {
-        this.x = x;
-        this.y = y;
+    constructor(posX, posY, couleur, text) {
+        this.posX = posX;
+        this.posY = posY;
         this.couleur = couleur;
         this.text = text;
     }
 
     changePlace(x, y) {
-        this.x = x;
-        this.y = y;
+        this.posX = x;
+        this.posY = y;
     }
 
-    changeColor(coul) {
-        this.couleur = coul;
+    changeCouleur(color) {
+        this.couleur = color;
     }
 
     afficheTest() {
-        let monElem = document.createElement("div");
-        monElem.className = "notes";
+        let monElem;
+        let creation = false;
+
+        if (document.getElementById("item") == null) {
+            monElem = document.createElement("div");
+
+            creation = true;
+        } else {
+            monElem = document.getElementById("item");
+        }
+
+        monElem.className = "note";
+        monElem.id = "item";
         monElem.style.position = "fixed";
-        monElem.style.top = this.y + "px";
-        monElem.style.left = this.x + "px";
-        monElem.style.width = "250px";
-        monElem.style.height = "150px";
+        monElem.style.top = this.posY + "px";
+        monElem.style.left = this.posX + "px";
         monElem.style.backgroundColor = this.couleur;
-        monElem.style.padding = "5px";
         monElem.style.color = "black";
-        monElem.innerHTML = ` <h3> ${this.text} </h3><p>test postit</p>`;
-        document.body.appendChild(monElem);
+        monElem.innerHTML = ` 
+        <div class="post-title">
+            <h3> ${this.text} </h3>
+        </div>
+        <div class="post-body">
+            <p>Add text</p> 
+        </div>
+        <div class="post-footer">
+            <i class="fas fa-expand-arrows-alt move"></i>
+            <i class="fas fa-compress-alt resize"></i>
+            <i class="fas fa-edit edit"></i>
+            <i class="fas fa-trash-alt del"></i>
+        </div>   
+            `;
+
+        if (creation) {
+            document.body.appendChild(monElem);
+        }
+
+        /* Mouvement du postit */
+        document
+            .querySelector(".move")
+            .addEventListener("mousedown", function() {
+                mouvement = true;
+            });
+
+        /* Resize  postit */
+        document
+            .querySelector(".resize")
+            .addEventListener("mousedown", initResize, false);
+
+        //click enfoncer pour resize
+        function initResize(e) {
+            window.addEventListener("mousemove", Resize, false);
+            window.addEventListener("mouseup", stopResize, false);
+        }
+        //resize the element
+        function Resize(e) {
+            monElem.style.width = e.clientX - monElem.offsetLeft + "px";
+            monElem.style.height = e.clientY - monElem.offsetTop + "px";
+        }
+        //relachement de souris stop resize
+        function stopResize(e) {
+            window.removeEventListener("mousemove", Resize, false);
+            window.removeEventListener("mouseup", stopResize, false);
+        }
+
+        /* supr postit */
+        document.querySelector(".del").addEventListener("click", function() {
+            monElem.remove();
+            delPost();
+        });
+
+        /* edit le postit */
+        document.querySelector(".edit").addEventListener("click", function() {
+            let button = document.createElement("button");
+            button.innerHTML = "Confirm";
+            monElem.appendChild(button);
+            monElem.setAttribute("contenteditable", "true");
+
+            button.addEventListener("click", function() {
+                monElem.setAttribute("contenteditable", "false");
+                button.remove();
+            });
+        });
     }
 }
 
-monTest1 = new Postit(435, 100, "blue", "Post ");
-monTest2 = new Postit(435, 300, "red", "Post ");
-monTest3 = new Postit(435, 500, "yellow", "Post");
 
 
 
 
-
-
+/* Création des postits */
+/*
+postit1.addEventListener('click', function()
+{
+        zone.addEventListener('mousedown', function()
+        {
+            i = i + 1;
+            monTest = new PostIt(x, y, "white", "#BF1736", i);
+            monTest.affichePostit();
+        });
+});
+postit2.addEventListener('click', function()
+{
+    zone.addEventListener('mousedown', function()
+        {
+            monTest = new PostIt(x, y, "white", "#0D1440");
+            monTest.affichePostit();
+        });
+});
+postit3.addEventListener('click', function()
+{
+    zone.addEventListener('mousedown', function()
+        {
+            monTest = new PostIt(x, y, "white", "#1438A6");
+            monTest.affichePostit();
+        });
+}); */
 
  
--- js/fonct.js
+++ js/fonct.js
@@ -1,19 +1,44 @@
-const addRed = document.querySelector(".add-1");
-const addGreen = document.querySelector(".add-2");
+let mouvement = false;
+let postIT = new Array();
+let x;
+let y;
+const addBlue = document.querySelector(".add-1");
+const addRed = document.querySelector(".add-2");
 const addYellow = document.querySelector(".add-3");
+let move = document.querySelector(".fa-people-carry");
+let item = document.querySelector("#item");
 
-/* Add Events */
-addRed.addEventListener("click", function() {
+/* ajoutez un post it */
+addBlue.addEventListener("click", function() {
 
+    monTest1 = new Postit(375, 92, "blue", "Post ");
     monTest1.afficheTest();
+    postIT.push(monTest1);
+    console.log(postIT);
 });
 
-addGreen.addEventListener("click", function() {
-    monTest2.afficheTest();
 
+/* Mouvement click enfoncer et relacher */
+document.addEventListener("mousemove", e => {
+    x = e.clientX;
+    y = e.clientY;
 });
 
-addYellow.addEventListener("click", function() {
-    monTest3.afficheTest();
-
+document.addEventListener("mouseup", () => {
+    mouvement = false;
 });
+
+function refresh() {
+    if (mouvement) {
+        monTest1.changePlace(x, y);
+        monTest1.afficheTest();
+    }
+    setTimeout(refresh, 10);
+}
+refresh();
+
+
+
+function delPost() {
+    postIT.pop(monTest1);
+}
